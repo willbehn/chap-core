e897649c0d93b3be4399c5d05816eada90e97ce5
William Henrik Behn <behnwilliam@icloud.com>
2026-09-15 12:00:49 +0200

    add value function for kernel shap that creates pertubations from masks and calls models predict

--- chap_core/explainability/kernel_shap.py
+++ chap_core/explainability/kernel_shap.py
@@ -1,5 +1,7 @@
 import logging
 import time
+import numpy as np
+import shap
 
 logger = logging.getLogger(__name__)
 
@@ -8,7 +10,9 @@ from chap_core.spatio_temporal_data.temporal_dataclass import DataSet
 
 from chap_core.explainability.lime import (
     prepare_explain_inputs,
-    build_feature_map
+    build_feature_map, 
+    perturb_vectors,
+    predict_pertubations
 )
 
 """
@@ -23,7 +27,7 @@ Sende dette inn i KernelExplainer med 0 som background, og 1 som på
 # Some of the samplers for lime might not work as intended for shap, will update
 # with more samplers
 def _check_allowed_sampler(sampler_name: str):
-    allowed_samplers = {"global_mean", "background", "random"}
+    allowed_samplers = {"global_mean"} # TODO expand with "background", "random" later
 
     assert sampler_name in allowed_samplers, (
         "Sampler not supported"
@@ -67,6 +71,48 @@ def explain_shap(
     print(f"inputs AFTER prepare_explain_inputs:\n{inputs}\n")
 
     feature_map = build_feature_map(inputs.x0)
+    num_features = len(feature_map)
 
     print(f"feature_map AFTER build_feature_map:\n{feature_map}\n")
+
+
+    def value_fn(masks: np.ndarray) -> np.ndarray:
+        print(masks)
+
+        perturbations, perturbation_masks = perturb_vectors(
+                inputs.hist_df, 
+                inputs.x0, 
+                inputs.feat_indices, 
+                inputs.sampler, 
+                feature_map, 
+                masks, 
+                lobal_means
+            )
+
+        _, y, _, _ = predict_pertubations(
+            model,
+            inputs.hist_df,
+            inputs.future_df,
+            perturbations,
+            perturbation_masks,
+            feature_names,
+            inputs.features_hist,
+            inputs.features_fut,
+            horizon,
+            location,
+            inputs.feat_indices,
+            inputs.hist_type,
+            inputs.fut_type,
+            dataset,
+            inputs.full_future_weather,
+        )
+
+        return np.array(y)
+
+
+
+    background = np.zeroes((1,num_features))
+    instance = np.ones((1, num_features))
+
+    explainer = shap.KernelExplainer(value_fn, background)
   
