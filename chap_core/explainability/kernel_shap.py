44f353c50a3f52936758bdf6ec1b502db4439d13
William Henrik Behn <behnwilliam@icloud.com>
2026-09-17 10:26:31 +0200

    Refactor kernel shap implementation. Add first iteration of pruning based on timeshap paper, not done yet, treats all features in old independently

--- chap_core/explainability/kernel_shap.py
+++ chap_core/explainability/kernel_shap.py
@@ -4,16 +4,18 @@ import time
 import numpy as np
 import shap
 
-logger = logging.getLogger(__name__)
-
 from chap_core.explainability.lime import (
+    ExplainInputs,
     build_feature_map,
     perturb_vectors,
     predict_pertubations,
     prepare_explain_inputs,
 )
+from chap_core.explainability.segment import SplitSegmentation
 from chap_core.models.external_model import ExternalModel
 from chap_core.spatio_temporal_data.temporal_dataclass import DataSet
+
+logger = logging.getLogger(__name__)
 
 """
 TODO fjern etterhvert
@@ -42,40 +44,22 @@ def _explain_shap(
     dataset: DataSet,
     location: str,
     horizon: int,
-    granularity: int,
     num_perturbations: int,
-    segmenter_name: str,
-    sampler_name: str,
-    last_n: int | None,
     seed: int | None,
-    timed: bool,
-    start: float,
+    inputs: ExplainInputs,
 ) -> list[tuple[str, float]]:
 
-    inputs = prepare_explain_inputs(
-        dataset=dataset,
-        location=location,
-        horizon=horizon,
-        segmenter_name=segmenter_name,
-        granularity=granularity,
-        sampler_name=_check_allowed_sampler(sampler_name),
-        seed=seed,
-        last_n=last_n,
-        timed=timed,
-        start=start,
-    )
-
-    print(f"inputs AFTER prepare_explain_inputs:\n{inputs}\n")
+    #print(f"inputs AFTER prepare_explain_inputs:\n{inputs}\n")
 
     feature_map = build_feature_map(inputs.x0)
     num_features = len(feature_map)
     feature_names = [name for name, _, _ in feature_map]
 
-    print(f"\n\nNUMBER OF FEATURES: {num_features}\n\n")
-    print(f"feature_map AFTER build_feature_map:\n{feature_map}\n")
+    #print(f"\n\nNUMBER OF FEATURES: {num_features}\n\n")
+    #print(f"feature_map AFTER build_feature_map:\n{feature_map}\n")
 
     def value_fn(masks: np.ndarray) -> np.ndarray:
-        print(masks)
+        #print(masks)
 
         perturbations, perturbation_masks = perturb_vectors(
             inputs.hist_df,
@@ -148,19 +132,27 @@ def explain(
         dataset, last_n = prune_temporal_depth(model, dataset, location)
     """
 
+    inputs = prepare_explain_inputs(
+        dataset=dataset,
+        location=location,
+        horizon=horizon,
+        segmenter=segmenter_name,
+        granularity=granularity,
+        sampler_name=_check_allowed_sampler(sampler_name),
+        seed=seed,
+        last_n=last_n,
+        timed=timed,
+        start=start,
+    )
+
     result = _explain_shap(
         model=model,
         dataset=dataset,
         location=location,
         horizon=horizon,
-        granularity=granularity,
         num_perturbations=num_perturbations,
-        segmenter_name=segmenter_name,
-        sampler_name=sampler_name,
-        last_n=last_n,
         seed=seed,
-        timed=timed,
-        start=start,
+        inputs=inputs,
     )
 
     end = time.perf_counter()
@@ -170,7 +162,21 @@ def explain(
     return result
 
 # Pruning strategy based on TimeSHAP, prunes the depth of the dataset
-def prune_temporal_depth(model: ExternalModel, dataset: DataSet, location: str):
+def prune_temporal_depth(
+    *,
+    model: ExternalModel,
+    dataset: DataSet,
+    location: str,
+    horizon: int,
+    granularity: int,
+    num_perturbations: int,
+    sampler_name: str,
+    last_n: int | None,
+    seed: int | None,
+    timed: bool,
+    start: float,
+    threshold: float):
+
     """
     psudokode:
     for i in {max_depth-1, max_depth-2 osv til}:
@@ -186,10 +192,52 @@ def prune_temporal_depth(model: ExternalModel, dataset: DataSet, location: str):
     """
 
     max_depth = len(dataset.period_range)
+    print(f"STARTING TEMPORAL PRUNING, max depth={max_depth}")
 
     # Ranges fdrom max_depth -1 to 1
-    for i in range(max_depth - 1, 0, -1):
-        period_range = dataset.period_range
+    for split_index in range(max_depth - 1, 0, -1):
 
-        new = dataset.restrict_time_period(slice(period_range[-1], None))
-        old = dataset.restrict_time_period(slice(None, period_range[-i, 1]))
+        """
+        TODO regarding segmentation REMEBER TO REMOVE
+        I need to pass down arguemtns to the segmenter from this call since I will need to call it multiple times.
+        My solution now is to change segmetner to also accept a segmentation model.
+        Issues: Comlpicated code, and I need to rerun data preperation for each time I want to change the segmentation
+        Solution: I will refactor it later to first do data prerperation -> call segment() that does the segmentation. 
+        """
+        inputs = prepare_explain_inputs(
+            dataset=dataset,
+            location=location,
+            horizon=horizon,
+            segmenter=SplitSegmentation(split_index),
+            granularity=granularity,
+            sampler_name=_check_allowed_sampler(sampler_name),
+            seed=seed,
+            last_n=last_n,
+            timed=timed,
+            start=start
+        )
+
+        result = _explain_shap(
+            model=model,
+            dataset=dataset,
+            location=location,
+            horizon=horizon,
+            num_perturbations=num_perturbations,
+            seed=seed,
+            inputs=inputs,
+        )
+
+        # TODO uses only the first feature as of now
+        old_seg = next((seg, sv) for seg, sv in result if "_seg_1" in seg)
+        new_seg = next((seg, sv) for seg, sv in result if "_seg_0" in seg)
+
+        print(f"OLD SEGMENT for split_index {split_index}: {old_seg}")
+
+        old_share = abs(old_seg[1])/ (abs(old_seg[1]) + abs(new_seg[1]))
+        print(f"SHARE: {old_share}")
+
+        if old_share < threshold:
+            return split_index
+
+    return 0
+        
